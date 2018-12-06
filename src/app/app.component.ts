cde0db74158d36f45c7e20163338f0699374e9b6
Eugene Khristo <marteau9292@gmail.com>
2018-12-06 18:35:19 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'HR-APP';
+}
