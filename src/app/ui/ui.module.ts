3c3fe7f539081ab185ba5f162d23176ca8c336be
Eugene Khristo <marteau9292@gmail.com>
2018-12-20 12:44:14 +0300

    Add dragndrop and resize features

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -4,7 +4,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from './material/material.module';
 import { SnackMessageComponent } from './components/snack-message/snack-message.component';
-import { HrCalendarComponent } from './components/calendar/hr-calendar.component';
 
 import { SnackMessagePbService } from './services/snack-message-pb.service';
 import { SnackMessageService } from './services/snack-messgae.service';
@@ -13,11 +12,11 @@ import { InterviewDialogModule } from './modules/interview-dialog/interview-dial
 import { InterviewDialogComponent } from './modules/interview-dialog/interview-dialog/interview-dialog.component';
 import { InterviewDialogService } from './modules/interview-dialog/interview-dialog.service';
 import { NewCandidateDialogComponent } from './modules/new-candidate-dialog/new-candidate-dialog/new-candidate-dialog.component';
-import {NewCandidateDialogModule} from './modules/new-candidate-dialog/new-candidate-dialog.module';
-import {NewCandidateDialogService} from './modules/new-candidate-dialog/new-candidate-dialog.service';
+import { NewCandidateDialogModule } from './modules/new-candidate-dialog/new-candidate-dialog.module';
+import { NewCandidateDialogService } from './modules/new-candidate-dialog/new-candidate-dialog.service';
 
 @NgModule({
-  declarations: [SnackMessageComponent, HrCalendarComponent],
+  declarations: [SnackMessageComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
@@ -26,13 +25,21 @@ import {NewCandidateDialogService} from './modules/new-candidate-dialog/new-cand
     InterviewDialogModule,
     NewCandidateDialogModule
   ],
-  providers: [SnackMessagePbService, SnackMessageService, InterviewDialogService, NewCandidateDialogService],
-  entryComponents: [SnackMessageComponent, InterviewDialogComponent, NewCandidateDialogComponent],
+  providers: [
+    SnackMessagePbService,
+    SnackMessageService,
+    InterviewDialogService,
+    NewCandidateDialogService
+  ],
+  entryComponents: [
+    SnackMessageComponent,
+    InterviewDialogComponent,
+    NewCandidateDialogComponent
+  ],
   exports: [
     MaterialModule,
     ReusableMatConfirmModule,
     FlexLayoutModule,
-    HrCalendarComponent,
     InterviewDialogModule,
     NewCandidateDialogModule
   ]
