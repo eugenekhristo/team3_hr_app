915da83be7493c088bc4537bf686071da1a42583
Eugene Khristo <marteau9292@gmail.com>
2018-12-23 13:19:54 +0300

    removed candidates and vacancy pages

--- src/app/pages/system/system-page.module.ts
+++ src/app/pages/system/system-page.module.ts
@@ -11,21 +11,8 @@ import { InterviewComponent } from './interview/containers/interview/interview.c
 import { InterviewListComponent } from './interview/presentationals/interview-list/interview-list.component';
 import { InterviewCardComponent } from './interview/presentationals/interview-card/interview-card.component';
 import { FullCalendarModule } from 'ng-fullcalendar';
-import { VacancyListComponent } from './vacancy/containers/vacancy-list/vacancy-list.component';
-import { VacancyEditComponent } from './vacancy/containers/vacancy-edit/vacancy-edit.component';
-import { VacancyViewComponent } from './vacancy/containers/vacancy-view/vacancy-view.component';
-import { AddCandidateComponent } from './vacancy/containers/vacancy-edit/add-candidate/add-candidate.component';
-import { MaterialModule } from '../../ui/material/material.module';
-import { AddSkillComponent } from './vacancy/containers/vacancy-edit/add-skill/add-skill.component';
-import { VacancyService } from './vacancy/vacancy.service';
 
-import { CandidateCardComponent } from './candidates/candidate-card/candidate-card.component';
-import { CandidatePageComponent } from './candidates/candidate-page/candidate-page.component';
-import { ShortInfoComponent } from './candidates/candidate-page/short-info/short-info.component';
-import { TimelineComponent } from './candidates/candidate-page/timeline/timeline.component';
-import { NewCvDialogComponent } from './candidates/candidate-page/timeline/new-cv-dialog/new-cv-dialog.component';
-import { NewExperienceDialogComponent } from './candidates/candidate-page/timeline/new-experience-dialog/new-experience-dialog.component';
-import { NewNotesDialogComponent } from './candidates/candidate-page/timeline/new-notes-dialog/new-notes-dialog.component';
+
 import { InterviewingComponent } from './interview/containers/interviewing/interviewing.component';
 import { InterviewStore } from './shared/services/interview-store.service';
 
@@ -35,18 +22,6 @@ import { InterviewStore } from './shared/services/interview-store.service';
     InterviewComponent,
     InterviewListComponent,
     InterviewCardComponent,
-    VacancyListComponent,
-    VacancyEditComponent,
-    VacancyViewComponent,
-    AddCandidateComponent,
-    CandidateCardComponent,
-    CandidatePageComponent,
-    ShortInfoComponent,
-    TimelineComponent,
-    NewCvDialogComponent,
-    NewExperienceDialogComponent,
-    NewNotesDialogComponent,
-    AddSkillComponent,
     InterviewingComponent
   ],
   imports: [
@@ -55,17 +30,10 @@ import { InterviewStore } from './shared/services/interview-store.service';
     ReactiveFormsModule,
     UiModule,
     SystemRoutingModule,
-    MaterialModule,
     FullCalendarModule
   ],
   exports: [SystemShellComponent],
-  providers: [InterviewService, InterviewStore, VacancyService],
-  entryComponents: [
-    NewExperienceDialogComponent,
-    NewNotesDialogComponent,
-    NewCvDialogComponent,
-    AddSkillComponent,
-    AddCandidateComponent
-  ]
+  providers: [InterviewService, InterviewStore],
+  entryComponents: []
 })
 export class SystemPageModule {}
